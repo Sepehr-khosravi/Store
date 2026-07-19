0a08aef555465d5a5c84ddbf05cd96ffb0b5efb2
=Sepehr-Khosravi <=>
2026-07-20 00:06:17 +0330

    finished the course what i was watching.

---
+++ src/main/java/com/project/store/PaymentService.java
@@ -0,0 +1,5 @@
+package com.project.store;
+
+public interface PaymentService {
+    void processPayment(double amount);
+}
--- src/main/java/com/project/store/StoreApplication.java
+++ src/main/java/com/project/store/StoreApplication.java
@@ -1,5 +1,6 @@
 package com.project.store;
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+		var orderService = context.getBean(OrderService.class);
+//		var orderService = new OrderService(new PayPalPaymentService());
+		orderService.placeOrder();
+
 	}
 
 }
